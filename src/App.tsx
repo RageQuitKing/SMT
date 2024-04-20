aa701a6392d80734186bf79d2218c92efdf4995f
Spanishh <emilexremondi@gmail.com>
2024-04-20 13:26:49 -0400

    Dug up Carets

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ function App() {
 	return (
 		<div className="App">
 			<Helmet>
-				<link
+				{/* <link
 					rel="stylesheet"
-					href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-				/>
+					href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min."
+				/> */}
 			</Helmet>
 			<Sidebar />
 		</div>
